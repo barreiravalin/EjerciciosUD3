61fc1aab5317f57d1736c22f88318e4bdaf24979
alberto <barreiravalin@gmail.com>
2025-12-03 10:53:31 +0100

    Ejercicios primera parte

--- src/ejerciciosUD3/Ejercicio11.java
+++ src/ejerciciosUD3/Ejercicio11.java
@@ -4,31 +4,31 @@ public class Ejercicio11 {
 
 // Es la solución pedida, pero es ineficiente.
     
-//    private static boolean esPerfecto(int n){
-//        int suma = 1;
-//        for (int i = 2; i < n; i++) 
-//            if (n % i == 0)
-//                suma += i;
-//        
-//        return suma == n;
-//    }
-    
     private static boolean esPerfecto(int n){
-        int suma = 1;
-        
-        for (int i = 2; i * i < n ; i++) {
-            if (n % i == 0){
+        int suma = 0;
+        for (int i = 1; i < n; i++) 
+            if (n % i == 0)
                 suma += i;
-                if (i != n / i)
-                    suma += n / i;
-            }
-        }
         
-        return n != 1 && suma == n;
+        return suma == n;
     }
     
+//    private static boolean esPerfecto(int n){
+//        int suma = 1;
+//        
+//        for (int i = 2; i * i < n ; i++) {
+//            if (n % i == 0){
+//                suma += i;
+//                if (i != n / i)
+//                    suma += n / i;
+//            }
+//        }
+//        
+//        return n != 1 && suma == n;
+//    }
+    
     public static void main(String[] args) {
-        final int MAX = 5;
+        final int MAX = 4;
         int n = 1;
         int contador = 0;
         
